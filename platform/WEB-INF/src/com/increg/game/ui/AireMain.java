486d13d21f043ba45b25e8d837202cfa6bc2cfea
Emmanuel Guyot <emmguyot@users.sourceforge.net>
2010-09-26 19:55:06 +0000

    Version

--- platform/WEB-INF/src/com/increg/game/ui/AireMain.java
+++ platform/WEB-INF/src/com/increg/game/ui/AireMain.java
@@ -240,9 +240,9 @@ public class AireMain extends Applet {
         super.paint(g);
         if ((model == null) || (model.getEtat() != AireMainModel.ETAT_OK)) {
             g.drawString("Chargement en cours, veuillez patienter ...", 20, 130);
-            g.drawString("InCrEG Online gaming platform V1.3 http://online-gaming.sourceforge.net", 20, 410);
+            g.drawString("InCrEG Online gaming platform V1.4 http://online-gaming.sourceforge.net", 20, 410);
             g.drawString("Logiciel sous licence GPL (http://www.opensource.org)", 20, 430);
-            g.drawString("Copyright 2003-2005 Valérie et Emmanuel Guyot", 20, 450);
+            g.drawString("Copyright 2003-2010 Valérie et Emmanuel Guyot", 20, 450);
             // g.drawString("Version modifiée par XXXX Copyright 2003-2005", 20, 470);
             g.drawString("Ce logiciel n'offre ABSOLUMENT AUCUNE GARANTIE", 20, 490);
             g.drawString("Ce logiciel est gratuit et nous vous encourageons à le redistribuer selon les termes de la licence GPL", 20, 510);
